b6952b8d92d00ef2eb15f3fd9969846245c1dc6e
ail2t <>
2026-07-22 23:14:24 +0200

    ajout de l'authentification

--- auth.py
+++ auth.py
@@ -2,11 +2,28 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url = "http://localhost:4280/"
-response = requests.get(url, timeout=10)
-html = response.text
-soup = BeautifulSoup(html, "html.parser")
+LOGIN_URL="http://localhost:4280/login.php"
+USER="admin"
+PASS="password"
+
+s = requests.Session()
+response = s.get(LOGIN_URL, timeout=10)
+soup = BeautifulSoup(response.text, "html.parser")
 
 token = soup.find("input", attrs={"name": "user_token"})["value"]
 
-print(token)
+print(f"Token récupéré : {token}")
+
+payload = {
+        "username":USER,
+        "password": PASS,
+        "Login": "Login",
+        "user_token": token
+        }
+
+r = s.post(LOGIN_URL, data=payload, timeout=10)
+
+if "Logout" in r.text:
+    print("Connexion reussie")
+else:
+    print("Connexion refusée")

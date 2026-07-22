e662ace058ab5b0a9b2cc4cf0da515c48c6c91e0
ail2t <>
2026-07-22 23:14:24 +0200

    début de l'implémentation pydantic

--- agent.py
+++ agent.py
@@ -1,5 +1,25 @@
 import anthropic
 
+from pydantic import BaseModel
+
+class Fiche(BaseModel):
+    """Champs fournis par l'agent"""
+    # Description de la vuln
+    description: str
+    # L'injection qui correspond à la vuln
+    payload: str
+    # La chaine qui prouve le succes de l'exploit
+    marqueur_succes: str
+    # Les paramètres annexes comme submit pour la SQLi
+    params_base: dict = {}
+    # Puis les champs fournis manuellement
+    base_url: str = ""
+    dvwa_security: str = ""
+    # Le chemin du module à tester
+    actif: str = ""
+    # Le paramètre à injecter dans le module
+    param_injecte: str = ""
+
 client = anthropic.Anthropic()
 
 message = client.messages.create(
